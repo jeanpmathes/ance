6685cb8644637cec9053908941bf1bee795e340c
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-02-07 15:18:23 +0100

    cmake not working

---
+++ cmake-build-debug/ance/CMakeFiles/ance.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/jeanp/source/repos/ance/ance/Application.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/Application.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/ArrayType.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/ArrayType.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/CompileState.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/CompileState.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/Constant.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/Constant.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/Function.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/Function.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/GlobalVariable.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/GlobalVariable.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/IntegerType.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/IntegerType.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/Scope.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/Scope.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/Statement.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/Statement.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/Value.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/Value.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/Visitor.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/Visitor.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/ance.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/ance.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/anceBaseVisitor.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/anceBaseVisitor.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/anceCompiler.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/anceCompiler.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/anceLexer.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/anceLexer.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/anceParser.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/anceParser.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/anceVisitor.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/anceVisitor.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/assignment_statement.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/assignment_statement.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/default_value_expression.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/default_value_expression.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/expression_statement.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/expression_statement.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/function_call.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/function_call.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/integer_expression.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/integer_expression.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/literal_expression.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/literal_expression.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/print_statement.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/print_statement.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/return_statement.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/return_statement.cpp.obj"
+  "C:/Users/jeanp/source/repos/ance/ance/variable_expression.cpp" "C:/Users/jeanp/source/repos/ance/cmake-build-debug/ance/CMakeFiles/ance.dir/variable_expression.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNICODE"
+  "_CONSOLE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
