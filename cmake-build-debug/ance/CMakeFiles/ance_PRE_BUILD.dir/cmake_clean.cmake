d1d2a34e599613e0a91e97ed92ed6733d90dd422
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-04-11 17:21:33 +0200

    Reorganization.

--- cmake-build-debug/ance/CMakeFiles/ance_PRE_BUILD.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ance_PRE_BUILD"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ance_PRE_BUILD.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
