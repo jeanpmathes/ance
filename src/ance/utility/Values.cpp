7a45fec0c852f2b4b9f43608772d8c26d4d9962c
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-05-24 18:14:48 +0200

    Add conversion methods for native<->content value conversion.

---
+++ src/ance/utility/Values.cpp
@@ -0,0 +1,53 @@
+#include "Values.h"
+
+#include "Value.h"
+#include "Type.h"
+#include "CompileState.h"
+
+llvm::Value* ance::Values::nativeToContent(
+	ance::Type* type,
+	llvm::Value* native,
+	llvm::LLVMContext& c,
+	llvm::Module*,
+	CompileState*,
+	llvm::IRBuilder<>& ir,
+	llvm::DIBuilder*
+)
+{
+	switch (type->storage())
+	{
+		case InternalStorage::AS_TEMPORARY:
+		{
+			return native;
+		}
+		case InternalStorage::AS_POINTER:
+		{
+			return ir.CreateLoad(native);
+		}
+	}
+}
+
+llvm::Value* ance::Values::contentToNative(
+	ance::Type* type,
+	llvm::Value* content,
+	llvm::LLVMContext& c,
+	llvm::Module*,
+	CompileState*,
+	llvm::IRBuilder<>& ir,
+	llvm::DIBuilder*
+)
+{
+	switch (type->storage())
+	{
+		case InternalStorage::AS_TEMPORARY:
+		{
+			return content;
+		}
+		case InternalStorage::AS_POINTER:
+		{
+			llvm::Value* native = ir.CreateAlloca(type->getNativeType(c));
+			ir.CreateStore(content, native);
+			return native;
+		}
+	}
+}
---
+++ src/ance/utility/Values.h
@@ -0,0 +1,24 @@
+#ifndef ANCE_SRC_ANCE_UTILITY_VALUES_H_
+#define ANCE_SRC_ANCE_UTILITY_VALUES_H_
+
+#include <llvm/IR/Value.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/DIBuilder.h>
+
+class CompileState;
+
+namespace ance
+{
+
+class Type;
+
+class Values
+{
+	public:
+		static llvm::Value* nativeToContent(ance::Type* type, llvm::Value* native, llvm::LLVMContext& c, llvm::Module* m, CompileState* state, llvm::IRBuilder<>& ir, llvm::DIBuilder* di);
+
+		static llvm::Value* contentToNative(ance::Type* type, llvm::Value* content, llvm::LLVMContext& c, llvm::Module* m, CompileState* state, llvm::IRBuilder<>& ir, llvm::DIBuilder* di);
+};
+}
+
+#endif
