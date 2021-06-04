837a22a7962a9b37234d3bcceec145c4ae324bf7
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-06-04 20:08:17 +0200

    Add AssignableExpression interface.

---
+++ src/ance/expression/AssignableExpression.h
@@ -0,0 +1,11 @@
+#ifndef ANCE_SRC_ANCE_EXPRESSION_ASSIGNABLEEXPRESSION_H_
+#define ANCE_SRC_ANCE_EXPRESSION_ASSIGNABLEEXPRESSION_H_
+
+#include "Expression.h"
+
+class AssignableExpression : public Expression
+{
+
+};
+
+#endif
