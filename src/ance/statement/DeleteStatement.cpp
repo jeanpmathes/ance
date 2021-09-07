af95c3eb14f9d66731ba034caae6c254f8aafdf9
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2021-09-07 19:07:02 +0200

    Validate values passed to delete.

--- src/ance/statement/DeleteStatement.cpp
+++ src/ance/statement/DeleteStatement.cpp
@@ -2,6 +2,7 @@
 
 #include "ance/construct/DefinedFunction.h"
 #include "ance/scope/LocalScope.h"
+#include "ance/type/PointerType.h"
 
 DeleteStatement::DeleteStatement(Expression* to_delete, bool delete_buffer, ance::Location location)
     : Statement(location)
@@ -16,7 +17,15 @@ void DeleteStatement::setFunction(ance::DefinedFunction* function)
 
 void DeleteStatement::validate(ValidationLogger& validation_logger)
 {
-    to_delete_->validate(validation_logger);
+    if (to_delete_->validate(validation_logger))
+    {
+        if (!ance::PointerType::isPointerType(to_delete_->type()))
+        {
+            validation_logger.logError("Value of type '" + to_delete_->type()->getName()
+                                           + "' given to 'delete' cannot be used as pointer type",
+                                       location());
+        }
+    }
 }
 
 void DeleteStatement::doBuild(CompileContext* context)
