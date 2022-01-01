68f4f940902f3db2a8de11a7268ca0d008a98688
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2022-01-01 19:48:36 +0100

    Add floating point operators

--- src/ance/type/FloatingPointType.cpp
+++ src/ance/type/FloatingPointType.cpp
@@ -1,6 +1,8 @@
 #include "FloatingPointType.h"
 
+#include "ance/construct/value/WrappedNativeValue.h"
 #include "ance/type/Type.h"
+#include "ance/utility/Values.h"
 #include "compiler/CompileContext.h"
 
 std::string ance::FloatingPointType::createMangledName()
@@ -22,4 +24,66 @@ llvm::DIType* ance::FloatingPointType::createDebugType(CompileContext* context)
 bool ance::FloatingPointType::isFloatingPointType() const
 {
     return true;
+}
+
+bool ance::FloatingPointType::isOperatorDefined(BinaryOperator, ance::ResolvingHandle<ance::Type> other)
+{
+    if (other->isFloatingPointType())
+    {
+        auto other_type = dynamic_cast<FloatingPointType*>(other->getActualType()->getDefinition());
+        return this == other_type;
+    }
+
+    return false;
+}
+
+ance::ResolvingHandle<ance::Type> ance::FloatingPointType::getOperatorResultType(BinaryOperator,
+                                                                                 ance::ResolvingHandle<ance::Type>)
+{
+    return self();
+}
+
+bool ance::FloatingPointType::validateOperator(BinaryOperator,
+                                               ance::ResolvingHandle<ance::Type>,
+                                               ance::Location,
+                                               ance::Location,
+                                               ValidationLogger&)
+{
+    return true;
+}
+
+std::shared_ptr<ance::Value> ance::FloatingPointType::buildOperator(BinaryOperator         op,
+                                                                    std::shared_ptr<Value> left,
+                                                                    std::shared_ptr<Value> right,
+                                                                    CompileContext*        context)
+{
+    left->buildContentValue(context);
+    right->buildContentValue(context);
+
+    llvm::Value* left_value  = left->getContentValue();
+    llvm::Value* right_value = right->getContentValue();
+
+    llvm::Value* result;
+
+    switch (op)
+    {
+        case BinaryOperator::ADDITION:
+            result = context->ir()->CreateFAdd(left_value, right_value);
+            break;
+        case BinaryOperator::SUBTRACTION:
+            result = context->ir()->CreateFSub(left_value, right_value);
+            break;
+        case BinaryOperator::MULTIPLICATION:
+            result = context->ir()->CreateFMul(left_value, right_value);
+            break;
+        case BinaryOperator::DIVISION:
+            result = context->ir()->CreateFDiv(left_value, right_value);
+            break;
+        case BinaryOperator::MODULO:
+            result = context->ir()->CreateFRem(left_value, right_value);
+            break;
+    }
+
+    llvm::Value* native_result = ance::Values::contentToNative(self(), result, context);
+    return std::make_shared<ance::WrappedNativeValue>(self(), native_result);
 }
--- src/ance/type/FloatingPointType.h
+++ src/ance/type/FloatingPointType.h
@@ -19,6 +19,19 @@ namespace ance
 
         [[nodiscard]] bool isFloatingPointType() const override;
         [[nodiscard]] bool isFloatingPointType(size_t precision) const override = 0;
+
+        bool isOperatorDefined(BinaryOperator op, ance::ResolvingHandle<ance::Type> other) override;
+        ance::ResolvingHandle<ance::Type> getOperatorResultType(BinaryOperator                    op,
+                                                                ance::ResolvingHandle<ance::Type> other) override;
+        bool                              validateOperator(BinaryOperator                    op,
+                                                           ance::ResolvingHandle<ance::Type> other,
+                                                           ance::Location                    left_location,
+                                                           ance::Location                    right_location,
+                                                           ValidationLogger&                 validation_logger) override;
+        std::shared_ptr<ance::Value>      buildOperator(BinaryOperator         op,
+                                                        std::shared_ptr<Value> left,
+                                                        std::shared_ptr<Value> right,
+                                                        CompileContext*        context) override;
     };
 }
 #endif
